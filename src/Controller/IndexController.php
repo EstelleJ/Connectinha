2ed71437c50d5fb8bd566edf0158827c8ef2c154
EstelleJ <contact@estellejacquet.fr>
2021-11-16 16:20:23 +0100

    ADD Index template modifications with DB

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\HomeContent;
 use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,13 +13,15 @@ class IndexController extends AbstractController {
 	#[Route('/', name: 'index')]
 	public function index(): Response {
 
+		$content = $this->getDoctrine()->getRepository(HomeContent::class)->find(1);
 		$favourites = $this->getDoctrine()->getRepository(Product::class)->findBy([
 				                                                                          'active' => 1,
 				                                                                          'favourite' => 1
 		                                                                          ], ['id' => 'DESC'], 3);
 
 		return $this->render('index/index.html.twig', [
-				'favourites' => $favourites
+				'favourites' => $favourites,
+				'content' => $content
 		]);
 	}
 }
