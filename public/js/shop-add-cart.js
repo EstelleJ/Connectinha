993491236c2b891262b17c6cfccaa8bcd4fc7a68
Yoann Moreau <yoann.moreau@tutanota.com>
2021-11-23 18:08:17 +0100

    Check for existing products in cart

--- public/js/shop-add-cart.js
+++ public/js/shop-add-cart.js
@@ -1,16 +1,13 @@
-import {ajax} from './tools/functions.js';
-
 // ============================================================================
-// Class
+// Classes
 // ============================================================================
-
 class CartProduct {
 	constructor(id, mantra, quantity, price, weight) {
-		this.id     = id;
-		this.mantra = mantra;
+		this.id       = id;
+		this.mantra   = mantra;
 		this.quantity = quantity;
-		this.price = price;
-		this.weight = weight;
+		this.price    = price;
+		this.weight   = weight;
 	}
 }
 
@@ -23,79 +20,47 @@ const addCartButton = document.getElementById("addCart");
 // ============================================================================
 // Functions
 // ============================================================================
-
-
-
 async function addCart() {
 
-	const element = this;
-
-	let mantraSelected = document.getElementById('mantraSelect').value;
+	let mantraSelected   = document.getElementById('mantraSelect').value;
 	let quantitySelected = document.getElementById('number').value;
 
-	console.log(mantraSelected);
-	console.log(quantitySelected);
-
 	let productId = this.dataset.id;
-	let price = this.dataset.price;
-	let weight = this.dataset.weight;
-
-	// let formData = new FormData();
-	// formData.append('ajax-product-id', productId);
+	let price     = this.dataset.price;
+	let weight    = this.dataset.weight;
 
 	let arrayProducts = [];
 
-	let currentProduct = new CartProduct(productId, mantraSelected, quantitySelected, price, weight);
-
-	// arrayProducts.push(new CartProduct(productId, mantraSelected, quantitySelected, price, weight));
-	//
-	// console.log(arrayProducts);
+	let currentProduct = new CartProduct(productId, mantraSelected, parseInt(quantitySelected), price, weight);
 
 	let localStorageItems = localStorage.getItem('products');
-
-	console.log('storage cart:');
-	console.log(localStorageItems);
-	console.log('===========================');
 
 	if (!!localStorageItems) {
 		let cart = JSON.parse(localStorageItems);
 
-		console.log('===== produit actuel =====');
-		console.log(currentProduct);
-		console.log('==========================');
-
-		if(JSON.parse(localStorageItems).includes(currentProduct)){
-			alert('oui');
-		}
-		else {
-			alert('non');
+		let found = false;
+		for (const item of cart) {
+			if (item.id === currentProduct.id) {
+				item.quantity += currentProduct.quantity;
+				found = true;
+				break;
+			}
 		}
 
-		cart.push(new CartProduct(productId, mantraSelected, quantitySelected, price, weight));
+		if (!found) {
+			cart.push(currentProduct);
+		}
+
 		localStorage.setItem('products', JSON.stringify(cart));
 	}
 	else {
+		arrayProducts.push(currentProduct);
 		localStorage.setItem('products', JSON.stringify(arrayProducts));
 	}
-
-	console.log('new cart:');
-	console.log(localStorage.getItem('products'));
-	console.log('========================');
-
-
-	// const response = await ajax('POST', '/ajax/add-cart/', formData);
-	//if (response === 'ok') {
-	// Le premier paramètre est le nom de l'item, puis la valeur en second
-
-	//}
-
-
 }
 
 
 // ============================================================================
 // Event listeners
 // ============================================================================
-
 addCartButton.addEventListener('click', addCart);
-
