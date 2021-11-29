bcce6edce88658a37eb43fec7ada535c20ab5d9d
EstelleJ <contact@estellejacquet.fr>
2021-11-29 13:47:15 +0100

    WIP Products display in cart template

--- public/js/cart/cart.js
+++ public/js/cart/cart.js
@@ -5,7 +5,6 @@ const cartTable = document.getElementById('cart-table');
 
 let cartProducts = [];
 
-
 // ============================================================================
 // Functions
 // ============================================================================
@@ -13,6 +12,23 @@ function addCartRow(product) {
 	let row = document.createElement('cart-row');
 	row.dataset.product = JSON.stringify(product);
 	cartTable.appendChild(row);
+}
+
+displayProducts();
+
+function displayProducts(){
+	let productRows = document.getElementsByTagName("cart-row");
+
+	console.log(productRows);
+	// console.log(productRows.length);
+
+	// for(let i = 0; i < productRows.length; i++ ){
+	// 	console.log(productRows[i])
+	// }
+
+	// Array.from(productRows).forEach(function(row){
+	// 	console.log(row.length)
+	// });
 }
 
 // ============================================================================
